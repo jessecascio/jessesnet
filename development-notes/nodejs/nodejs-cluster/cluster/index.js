3f1dea8dbfdfa49c33111faa4eb503f61bacb243
Jesse Cascio <jesse.cascio@timeinc.com>
2017-04-02 19:19:29 -0700

    dep depr, comments

--- development-notes/nodejs/nodejs-cluster/cluster/index.js
+++ development-notes/nodejs/nodejs-cluster/cluster/index.js
@@ -4,16 +4,20 @@ var exectimer = require('exectimer');
 var fs = require('fs');
 
 if (cluster.isMaster) {
+  // track performance
   var timer = new exectimer.Tick('points');
   
+  // allow for cmd line args for worker / data sizes
   var args = args();
 
   var size = parseInt(args.s) || 10000;
   var workers = parseInt(args.w) || 1;
 
+  // read generated mock data
   var data = fs.readFileSync(`${__dirname}/../data/mock-data-${size}.json`, 'utf-8');
   var json = JSON.parse(data);
 
+  // limit workers to number of cores - 1
   if (workers > os.cpus().length - 1 || workers <= 0) {
     workers = os.cpus().length - 1;
   }
@@ -23,12 +27,15 @@ if (cluster.isMaster) {
   var distances = [];
   var finished = 0;
 
+  // parent event listeners to workers IPC data
   cluster.on('message', function(worker, message) {
+    // recieved data from worker via IPC
     distances.push(message.distance);
   });
   cluster.on('exit', function (worker, code, signal) {
     finished++;
 
+    // once parent dies workers will also die, wait for all to finish
     if (finished === workers) {
       timer.stop();
 
@@ -39,23 +46,27 @@ if (cluster.isMaster) {
 
   });
 
+  // calc segments for each worker to analyze
   var segment = Math.ceil(json.length / workers);
   var total = 0;
 
   timer.start();
 
+  // spawn workers
   for (var i=0; i<workers; i++) {
     var end = total + segment > json.length ? json.length : total + segment;
     cluster.fork().send({ points: json, start: total, end: end });
     total += segment;
   }
 } else {
+  // code worker processes run, recieve mesasge, process, respond, die
   process.on('message', function(message) {
     process.send({ distance: brute(message.points, message.start, message.end) });
     process.exit();
   });
 }
 
+// shared algorithm across all processes
 function brute(points, start, end) {
   var min = -1;
 
