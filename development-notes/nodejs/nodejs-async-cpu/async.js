bc36bd88395ec7ae225506d4265b48a319e75dd3
Jesse Cascio <jesse.cascio@timeinc.com>
2018-08-06 11:02:44 -0700

    node async/sync

---
+++ development-notes/nodejs/nodejs-async-cpu/async.js
@@ -0,0 +1,37 @@
+
+const LIMIT = 2000000000;
+
+for (let i=0; i<LIMIT; i++) { }
+
+
+/*
+function asyncAvg(n, avgCB) {
+  // Save ongoing sum in JS closure.
+  var sum = 0;
+  function help(i, cb) {
+    sum += i;
+    if (i == n) {
+      cb(sum);
+      return;
+    }
+
+    // "Asynchronous recursion".
+    // Schedule next operation asynchronously.
+    setImmediate(help.bind(null, i+1, cb));
+  }
+
+  // Start the helper, with CB to call avgCB.
+  help(1, function(sum){
+      var avg = sum/n;
+      avgCB(avg);
+  });
+}
+
+asyncAvg(n, function(avg){
+  console.log('avg of 1-n: ' + avg);
+});
+ */
+
+/*
+The fs.readFile() function buffers the entire file. To minimize memory costs, when possible prefer streaming via fs.createReadStream().
+ */
---
+++ development-notes/nodejs/nodejs-async-cpu/sync.js
@@ -0,0 +1,4 @@
+
+const LIMIT = 2000000000;
+
+for (let i=0; i<LIMIT; i++) { }
